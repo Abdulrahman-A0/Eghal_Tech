b0d1acfa59e7959f7eb08ef71c5040dd80d1dcfa
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2025-08-12 19:09:07 +0300

    some fixes and publishing test

--- EghalTech/Program.cs
+++ EghalTech/Program.cs
@@ -54,18 +54,18 @@ namespace EghalTech
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseRouting();
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.MapStaticAssets();
+            //app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            //.WithStaticAssets();
 
             using (var scope = app.Services.CreateScope())
             {
